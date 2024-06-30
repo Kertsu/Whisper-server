1c03b921c74d358a5a45c8221647258d3f9c92fb
Kertsu <kurtddbigtas@gmail.com>
2024-06-30 22:42:27 +0800

    add Google OAuth

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
 import helmet from "helmet";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import session from "express-session";
+import passport from "../config/passport.js";
 import userRouter from "./routes/userRoutes.js";
 import { success } from "./utils/httpResponse.js";
+import { connect } from "../config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+connect()
 
 app.get("hello-world", (req, res) => {
   success(res, { message: "Hello, world!" });
 });
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/'); 
+  }
+);
 
 app.use("/api/v1/users", userRouter);
 
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,20 +1,30 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Username is required"],
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    googleId: { type: String, unique: true, sparse: true },
+    avatar: { type: String },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
