f73488d03516cf203e61b5d821ff9d6a13bfaf67
Kertsu <kurtddbigtas@gmail.com>
2024-08-12 21:12:56 +0800

    feat: add refresh token model

---
+++ src/models/refreshToken.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const refreshTokenSchema = new mongoose.Schema(
+  {
+    token: { type: String, required: true, unique: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    expiresAt: { type: Date, required: true}
+  },
+  { timestamps: true }
+);
+
+const RefreshToken = mongoose.model("RefreshToken", refreshTokenSchema);
+
+export default RefreshToken;
