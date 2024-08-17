9b5f8888895a7375fe32dab9fe75f190bfde9061
Kertsu <kurtddbigtas@gmail.com>
2024-08-17 18:50:49 +0800

    fix: create report

--- src/controllers/report.controller.js
+++ src/controllers/report.controller.js
@@ -1,29 +1,33 @@
 import asyncHandler from "express-async-handler";
 import { error, success } from "../utils/httpResponse.js";
 import Report from "../models/report.model.js";
+import Conversation from "../models/conversation.model.js";
 
 export const createReport = asyncHandler(async (req, res) => {
-  const { reportType, conversationId, defendantId } = req.body;
-
-  if (!defendantId) {
-    return error(res, null, "Defendant not found", 404);
-  }
-
-  if (!reportType) {
-    return error(res, null, "Type not provided", 400);
-  }
+  const { reportType, conversationId } = req.body;
+  const requestingUserId = req.user._id;
 
   if (!conversationId) {
     return error(res, null, "Conversation not provided", 400);
   }
 
-  if (defendantId == req.user._id) {
-    return error(res, null, "Cannot report yourself", 403);
+  const conversation = await Conversation.findById(conversationId);
+
+  if (!conversation) {
+    return error(res, null, "Conversation not found", 404);
+  }
+
+  const defendantId = conversation.initiator.equals(requestingUserId)
+    ? conversation.recipient
+    : conversation.initiator;
+
+  if (!reportType) {
+    return error(res, null, "Type not provided", 400);
   }
 
   try {
     const report = await Report.create({
-      plaintiff: req.user._id,
+      plaintiff: requestingUserId,
       defendant: defendantId,
       reportType,
       conversation: conversationId,
